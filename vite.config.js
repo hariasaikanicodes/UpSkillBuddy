33f122ed7d906092fa23bbde64afd065dfd8cd5e
Hari Ramkumar R <hariramkumarr@Haris-MacBook-Air.local>
2026-09-24 20:32:48 +0530

    White Screen in Display Fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? '/AISkillBuddy/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/UpSkillBuddy/' : '/',
   build: {
     rollupOptions: {
       input: {
